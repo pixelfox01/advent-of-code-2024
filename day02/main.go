1c1bd58433ee3390128f55aa44939d79f3120328
Imran Yafith <imranyafith@gmail.com>
2024-12-03 11:33:21 -0500

    day02: part2 (done)

--- day02/main.go
+++ day02/main.go
@@ -32,7 +32,9 @@ func isSafe(levels []int) bool {
 	return true
 }
 
-func part1(f *os.File) {
+func part2() {
+	f, err := os.Open("part2.txt")
+	check(err)
 	var report string
 	var levels []int
 	var safeReports int
@@ -48,7 +50,44 @@ func part1(f *os.File) {
 			levels = append(levels, num)
 		}
 
-		if isSafe(levels) {
+		if safe := isSafe(levels); safe {
+			safeReports++
+		} else {
+			for i := range levels {
+				temp := append([]int(nil), levels[:i]...)
+				temp = append(temp, levels[i+1:]...)
+				safe = isSafe(temp)
+				if safe {
+					safeReports++
+					break
+				}
+			}
+		}
+		levels = nil
+	}
+
+	fmt.Println(safeReports)
+}
+
+func part1() {
+	f, err := os.Open("part1.txt")
+	check(err)
+	var report string
+	var levels []int
+	var safeReports int
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		report = scanner.Text()
+		fields := strings.Fields(report)
+		for _, f := range fields {
+			num, err := strconv.Atoi(f)
+			check(err)
+			levels = append(levels, num)
+		}
+
+		if safe := isSafe(levels); safe {
 			safeReports++
 		}
 
@@ -59,7 +98,6 @@ func part1(f *os.File) {
 }
 
 func main() {
-	f, err := os.Open("part1.txt")
-	check(err)
-	part1(f)
+	part1()
+	part2()
 }
